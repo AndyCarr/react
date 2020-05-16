7dd8556a77d171798db8f8f831a622dd295b0074
Andy Carr <andy@nextfriday.co.uk>
2020-05-16 09:36:32 +0100

    Simplifies

--- src/index.js
+++ src/index.js
@@ -1,15 +1,47 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React, { useEffect, useState } from "react";
+import ReactDOM from "react-dom";
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+
+function getWindowDimensions() {
+  const { innerWidth: width, innerHeight: height } = window;
+  return {
+    width,
+    height
+  };
+}
+
+export default function useWindowDimensions() {
+  const [windowDimensions, setWindowDimensions] = useState(
+    getWindowDimensions()
+  );
+
+  useEffect(() => {
+    function handleResize() {
+      setWindowDimensions(getWindowDimensions());
+    }
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  return windowDimensions;
+}
+
+const Component = () => {
+  const { height, width } = useWindowDimensions();
+
+  return (
+    <div>
+      <h1> Here be Dragons</h1>
+      
+      width: {width} ~ height: {height}
+    </div>
+  );
+};
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<Component />, rootElement);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
